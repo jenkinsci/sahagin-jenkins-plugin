f8fd4d26703a4eea4e7e00507a215f24ce0bf691
NozomiIto <nozomi.ito@trident-qa.com>
2015-09-22 18:20:46 +0900

    use sahagin-0.8.1 and fix compile error by temporal way

--- src/main/java/org/sahagin/jenkins/SahaginMainExecutor.java
+++ src/main/java/org/sahagin/jenkins/SahaginMainExecutor.java
@@ -1,19 +1,24 @@
 package org.sahagin.jenkins;
 
 
+import hudson.FilePath;
+import hudson.remoting.VirtualChannel;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.sahagin.main.SahaginMain;
 import org.sahagin.share.Config;
 import org.sahagin.share.IllegalDataStructureException;
 import org.sahagin.share.IllegalTestScriptException;
 import org.sahagin.share.yaml.YamlConvertException;
-
-import hudson.FilePath;
-import hudson.remoting.VirtualChannel;
+import org.sahagin.share.yaml.YamlUtils;
 
 public class SahaginMainExecutor implements FilePath.FileCallable<FilePath> {
+    // TODO constant for temporal logic
+    private static final String INVALID_CONFIG_YAML = "failed to load config file \"%s\": %s";
+    private static final long serialVersionUID = 1L;
     private String configAbsPath;
 
     public SahaginMainExecutor(String configAbsPath) {
@@ -32,8 +37,17 @@ public class SahaginMainExecutor implements FilePath.FileCallable<FilePath> {
         } catch (IllegalTestScriptException e) {
             throw new IOException(e);
         }
+        // TODO temporal logic. should define generateFromYamlConfig method on org.sahagin.share.Config
+        // and use it
         try {
-            Config config = Config.generateFromYamlConfig(file);
+            Map<String, Object> configYamlObj = YamlUtils.load(file);
+            Config config = new Config() {};
+            try {
+                config.fromYamlObject(configYamlObj);
+            } catch (YamlConvertException e) {
+                throw new YamlConvertException(String.format(
+                        INVALID_CONFIG_YAML, file.getAbsolutePath(), e.getLocalizedMessage()), e);
+            }                    
             FilePath reportOutputDir = new FilePath(config.getRootBaseReportOutputDir());
             return reportOutputDir;
         } catch (YamlConvertException e) {
